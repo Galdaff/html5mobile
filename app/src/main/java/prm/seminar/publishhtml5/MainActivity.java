82ebeef9ea5a8caeec1b73894d5016271a016d56
Thong Thanh <phanthongthanh0606@gmail.com>
2020-03-04 17:49:25 +0700

    setup for webView

--- app/src/main/java/prm/seminar/publishhtml5/MainActivity.java
+++ app/src/main/java/prm/seminar/publishhtml5/MainActivity.java
@@ -3,12 +3,18 @@ package prm.seminar.publishhtml5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webViewContainer);
+        webView.loadUrl("file:///android_asset/www/index.html");
     }
 }
