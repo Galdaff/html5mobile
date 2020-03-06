9f371a6a4c97b0f24e8e0c3d2058e5130ebe77e8
HaiDinh <haidn@unicode.edu.vn>
2020-03-06 20:12:22 +0700

    code do
    :)

--- app/src/main/java/prm/seminar/publishhtml5/MainActivity.java
+++ app/src/main/java/prm/seminar/publishhtml5/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webViewContainer);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/www/index.html");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/www/js/index.html");
     }
 }
